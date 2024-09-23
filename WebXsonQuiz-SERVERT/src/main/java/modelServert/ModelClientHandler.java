5249c458b6978f561f527f560bb10524ca6504a8
Benjamín de Jesús <benjaminperez201831504@cunoc.edu.gt>
2024-09-23 13:16:04 -0600

    Mejorar al servidor cliente

---
+++ WebXsonQuiz-SERVERT/src/main/java/modelServert/ModelClientHandler.java
@@ -0,0 +1,83 @@
+package modelServert;
+
+import com.cunoc.webxsonquiz.data.servert.Trivia;
+import java.util.ArrayList;
+import reactions.DataBaseListTrivia;
+import reactions.SystemAcess;
+import servlets.socket.ClientHandler;
+
+/**
+ *
+ * @author drymnz
+ */
+public class ModelClientHandler {
+
+    private ClientHandler client;
+
+    public ModelClientHandler(ClientHandler client) {
+        this.client = client;
+    }
+
+    private Boolean loginClient(Object get) {
+        if (get instanceof String) {
+            String text = (String) get;
+            if (text.isEmpty()) {
+                return false;
+            }
+            this.client.setUserclient((new SystemAcess(text)).loginSystem());
+            System.out.println(this.client.getUserclient().toString());
+            return this.client.getUserclient() != null;
+        }
+        return false;
+    }
+
+    public Object getResponse(Object getCliente) {
+        try {
+            // Retorna una nueva instancia del tipo T usando reflection
+            if (getCliente instanceof String) {
+                String text = (String) getCliente;
+                System.out.println(this.client.getClientSocket().getLocalAddress().getHostAddress() + "Peticion > " + text);
+                if (text.equals("false")) {
+                    this.client.setOutCliente(!this.client.isOutCliente());
+                    System.out.println("CERRAR a " + this.client.getClientSocket().getLocalAddress().getHostAddress());
+                    return false;
+                } else {
+                    boolean login = this.loginClient(text);
+                    String nameCliente = this.client.getClientSocket() != null ? this.client.getUserclient().getId() : this.client.getClientSocket().getLocalAddress().getHostAddress();
+                    if (login) {
+                        System.out.println("Entro: " + nameCliente);
+                        return this.client.getUserclient();
+
+                    } else {
+                        System.out.println("Intento de cliente: " + nameCliente);
+                        return false;
+                    }
+                }
+            } else if (getCliente instanceof ArrayList) {
+                this.client.setIndex(0);
+                this.client.setListTriviaDataBase((new DataBaseListTrivia().getListTrivias()));
+                if (this.client.getListTriviaDataBase().size() > this.client.getIndex()) {
+                    Trivia newTrivia = this.client.getListTriviaDataBase().get(this.client.getIndex());
+                    System.out.println("Le manda las trivias " + (this.client.getIndex() + 1) + "/" + this.client.getListTriviaDataBase().size() + " => " + this.client.getClientSocket().getLocalAddress().getHostAddress());
+                    return newTrivia;
+                } else {
+                    return false;
+                }
+            } else if (getCliente instanceof Trivia) {
+                this.client.setIndex(this.client.getIndex() + 1);
+                if (this.client.getListTriviaDataBase().size() > this.client.getIndex()) {
+                    Trivia newTrivia = this.client.getListTriviaDataBase().get(this.client.getIndex());
+                    System.out.println("Le manda las trivias " + (this.client.getIndex() + 1) + "/" + this.client.getListTriviaDataBase().size() + " => " + this.client.getClientSocket().getLocalAddress().getHostAddress());
+                    return newTrivia;
+                } else {
+                    return false;
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null; // Manejo de error
+        }
+    }
+
+}
--- WebXsonQuiz-SERVERT/src/main/java/servlets/socket/ClientHandler.java
+++ WebXsonQuiz-SERVERT/src/main/java/servlets/socket/ClientHandler.java
@@ -10,13 +10,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import reactions.DataBaseListTrivia;
-
-import reactions.SystemAcess;
+import modelServert.ModelClientHandler;
 
 /**
  *
@@ -29,24 +26,13 @@ public class ClientHandler implements Runnable {
     private int index = 0;
     private List<Trivia> listTriviaDataBase = null;
     private  boolean outCliente = false;
+    private ModelClientHandler model;
 
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        this.model = new ModelClientHandler(this);
     }
-
-    private Boolean loginClient(Object get) {
-        if (get instanceof String) {
-            String text = (String) get;
-            if (text.isEmpty()) {
-                return false;
-            }
-            this.userclient = (new SystemAcess(text)).loginSystem();
-            System.out.println(this.userclient.toString());
-            return this.userclient != null;
-        }
-        return false;
-    }
-
+    
     @Override
     public void run() {
         ObjectInputStream in = null;
@@ -61,57 +47,8 @@ public class ClientHandler implements Runnable {
                 System.out.println("----------------- ESPERANDO MENSAJE-----------------");
                 /* El mensaje que manda el cliente */
                 Object getCliente = in.readObject();
-
-                if (getCliente instanceof String) {
-                    String text = (String) getCliente;
-                    System.out.println(clientSocket.getLocalAddress().getHostAddress() + "Peticion > " + text);
-                    if (text.equals("false")) {
-                        this.outCliente = !this.outCliente;
-                        System.out.println("CERRAR a " + clientSocket.getLocalAddress().getHostAddress());
-                        out.writeObject(false);
-                        out.flush();
-                    } else {
-                        boolean login = this.loginClient(text);
-                        String nameCliente = this.clientSocket != null ? this.userclient.getId() : clientSocket.getLocalAddress().getHostAddress();
-                        if (login) {
-                            System.out.println("Entro: "+nameCliente);
-                            out.writeObject(this.userclient);
-                            out.flush();
-                        } else {
-                            System.out.println("Intento de cliente: "+nameCliente);
-                            out.writeObject(login);
-                            out.flush();
-                        }
-                    }
-                }else
-                if (getCliente instanceof ArrayList) {
-                    this.index = 0;
-                    this.listTriviaDataBase = (new DataBaseListTrivia().getListTrivias());
-                    if (this.listTriviaDataBase.size() > this.index) {
-                        Trivia newTrivia = listTriviaDataBase.get(this.index);
-                        System.out.println("Le manda las trivias " + (this.index + 1) + "/" + this.listTriviaDataBase.size() + " => " + clientSocket.getLocalAddress().getHostAddress());
-                        out.writeObject(newTrivia);
-                        out.flush();
-                    } else {
-                        out.writeObject(false);
-                        out.flush();
-                    }
-                }else
-                if (getCliente instanceof Trivia) {
-                    index++;
-                    if (this.listTriviaDataBase.size() > this.index) {
-                        Trivia newTrivia = listTriviaDataBase.get(this.index);
-                        System.out.println("Le manda las trivias " + (this.index + 1) + "/" + this.listTriviaDataBase.size() + " => " + clientSocket.getLocalAddress().getHostAddress());
-                        out.writeObject(newTrivia);
-                        out.flush();
-                    } else {
-                        out.writeObject(false);
-                        out.flush();
-                    }
-                }
-                /*                if (getCliente == null) {
-                outCliente = true;
-                }*/
+                out.writeObject(this.model.getResponse(getCliente));
+                out.flush();
                 System.out.println("----------------- REPUESTA-----------------");
             }
             System.out.println("----------------- SE DESCONECTO ALGUIEN -----------------");
@@ -128,4 +65,41 @@ public class ClientHandler implements Runnable {
             }
         }
     }
+    
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
+
+    public void setOutCliente(boolean outCliente) {
+        this.outCliente = outCliente;
+    }
+
+    public boolean isOutCliente() {
+        return outCliente;
+    }
+
+    public User getUserclient() {
+        return userclient;
+    }
+
+    public void setUserclient(User userclient) {
+        this.userclient = userclient;
+    }
+
+    public List<Trivia> getListTriviaDataBase() {
+        return listTriviaDataBase;
+    }
+
+    public void setListTriviaDataBase(List<Trivia> listTriviaDataBase) {
+        this.listTriviaDataBase = listTriviaDataBase;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
 }
