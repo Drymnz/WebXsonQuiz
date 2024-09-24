6afd8d80efdd829bce225a19066da696305d126d
Benjamín de Jesús <benjaminperez201831504@cunoc.edu.gt>
2024-09-24 13:24:18 -0600

    optimize el peso de los archivos

--- WebXsonQuiz-SERVERT/src/main/java/LexicalAndSyntacticAnalyzer/jflexandcupManagerUser/installJflexCup.sh
+++ WebXsonQuiz-SERVERT/src/main/java/LexicalAndSyntacticAnalyzer/jflexandcupManagerUser/installJflexCup.sh
@@ -8,9 +8,9 @@ nameLexema='LexemaByMyParesUser'
 
 rm -f "$nameLexema.java" 
 
-java -jar jflex-full-1.9.1.jar code.jflex 
+java -jar ../jflexandcup/jflex-full-1.9.1.jar code.jflex 
 
 rm -f "$nameClassCup.java" 
 rm -f "$nameSymCup.java" 
 
-java -jar java-cup-11b.jar -parser $nameClassCup -symbols $nameSymCup code.cup
+java -jar ../jflexandcup/java-cup-11b.jar -parser $nameClassCup -symbols $nameSymCup code.cup
